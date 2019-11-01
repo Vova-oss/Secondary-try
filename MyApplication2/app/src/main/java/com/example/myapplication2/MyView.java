f130f7fec41ab04f8de38530a4d461a8f763c191
Шизофреник <ogyrcka@gmail.com>
2019-11-01 10:22:20 +0300

    1.7.6.2 ondrawCanvas

---
+++ MyApplication2/app/src/main/java/com/example/myapplication2/MyView.java
@@ -0,0 +1,24 @@
+package com.example.myapplication2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(300, 300, 200, paint);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(300, 300, 200, paint);
+    }
+}
